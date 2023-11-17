ef02a4f13bc588d8d1f1d0cef3adb3678e3c5b55
FennecFromSahara <143624395+FennecFromSahara@users.noreply.github.com>
2023-11-17 22:57:11 +0100

    create network infrastructure

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("../Project/analog-reef-399320-e5f4d0d5ea9a.json")
+
+  project = "analog-reef-399320"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
