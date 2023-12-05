c0e60b0e8d29e07755eb89f94af277295b39993a
FennecFromSahara <143624395+FennecFromSahara@users.noreply.github.com>
2023-12-05 15:13:25 +0100

    test no provider credentials

--- src/main.tf
+++ src/main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "gcs" {
-    bucket = "portfolio-fennecfromsahara-bucket"
-    prefix = "terraform/state"
-    # credentials = "../../Project/analog-reef-399320-e5f4d0d5ea9a.json"
+    bucket      = "portfolio-fennecfromsahara-bucket"
+    prefix      = "terraform/state"
+    credentials = "../../Project/analog-reef-399320-e5f4d0d5ea9a.json"
   }
   required_providers {
     google = {
